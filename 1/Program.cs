924bf13222033299f70b8329a9163a3a20ae7f0d
Andreas Lengkeek <andreas.lengkeek@gmail.com>
2024-12-02 22:26:14 +1100

    day 1 and 2

---
+++ 1/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.WriteLine("Reading input >>>");
+var x = ReadInput("./input.txt");
+
+// GetDifference(x.l1, x.l2);
+GetSimilarity(x.l1, x.l2);
+
+
+static void GetSimilarity(List<int> l1, List<int> l2)
+{
+    var rightDict = new Dictionary<int, int>();
+    foreach (int l in l2)
+    {
+        if (rightDict.ContainsKey(l)) rightDict[l] += 1;
+        else rightDict.Add(l, 1);
+    }
+    Console.WriteLine($"Total unique nums: {rightDict.Keys.Count}");
+
+    var sum = 0;
+    foreach (int l in l1)
+    {
+        var count = rightDict.ContainsKey(l) ? rightDict[l] : 0;
+        sum += l * count;
+    }
+
+    Console.WriteLine($"Similarity score: {sum}");
+}
+static void GetDifference(List<int> l1, List<int> l2)
+{
+    var orderedLeft = l1.Order().ToArray();
+    var orderedRight = l2.Order().ToArray();
+
+    var sum = 0;
+    for (int i = 0; i < orderedLeft.Length; i++)
+    {
+        sum += Math.Abs(orderedLeft[i] - orderedRight[i]);
+    }
+
+    Console.WriteLine($"total diff: {sum}");
+}
+
+static (List<int> l1, List<int> l2) ReadInput(string input)
+{
+    List<int> l1 = new List<int>();
+    List<int> l2 = new List<int>();
+    String? line;
+    try
+    {
+        StreamReader sr = new StreamReader(input);
+        line = sr.ReadLine();
+        while (line != null)
+        {
+            var items = line.Split("  ");
+            l1.Add(Int32.Parse(items[0]));
+            l2.Add(Int32.Parse(items[1]));
+
+            line = sr.ReadLine();
+        }
+
+        sr.Close();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+
+    return (l1, l2);
+}
---
+++ 2/Program.cs
@@ -0,0 +1,92 @@
+﻿var reports = ReadInput("./input.txt");
+Console.WriteLine($"Total reports: {reports.Count}");
+
+var safeCount = 0;
+foreach (var report in reports)
+{
+    report.ForEach(x => Console.Write($"{x} "));
+
+    var isSafe = ReportIsSafe(report);
+    PrintReportStatus(isSafe);
+
+    if (isSafe)
+    {
+        safeCount++;
+    }
+    else
+    {
+        var isSafeB = false;
+        for (int i = 0; i < report.Count; i++)
+        {
+            var substituteReport = report.Where((item, index) => index != i).Select(x => x).ToList();
+            Console.Write('\t');
+            substituteReport.ForEach(x => Console.Write($"{x} "));
+            isSafeB = ReportIsSafe(substituteReport);
+            PrintReportStatus(isSafeB);
+            if (isSafeB) break;
+        }
+
+        if (isSafeB) safeCount++;
+    }
+}
+
+Console.WriteLine($"Safe reports: {safeCount}");
+
+static void PrintReportStatus(bool isSafe)
+{
+    Console.BackgroundColor = isSafe ? ConsoleColor.Green : ConsoleColor.Red;
+    Console.Write(isSafe ? "Safe" : "Unsafe");
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.WriteLine();
+}
+
+static bool ReportIsSafe(List<int> report)
+{
+    var isSafe = true;
+    var isAsc = report[0] < report[1];
+    var prevLevel = 0;
+    foreach (var level in report)
+    {
+        if (prevLevel != 0)
+        {
+            if (!isAsc && prevLevel > level && ((prevLevel - level) <= 3)) // desc gradual
+            {
+            }
+            else if (isAsc && prevLevel < level && ((level - prevLevel) <= 3)) // asc gradual
+            {
+            }
+            else
+            {
+                isSafe = false;
+            }
+        }
+
+        prevLevel = level;
+    }
+    return isSafe;
+}
+
+static List<List<int>> ReadInput(string filename)
+{
+    List<List<int>> input = new List<List<int>>();
+    String? line;
+    try
+    {
+        StreamReader sr = new StreamReader(filename);
+        line = sr.ReadLine();
+        while (line != null)
+        {
+            var report = line.Split(' ').Select(Int32.Parse).ToList();
+            input.Add(report);
+            line = sr.ReadLine();
+        }
+
+        sr.Close();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+
+    return input;
+}
